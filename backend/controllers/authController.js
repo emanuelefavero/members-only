f73941ec04afab500ab26f50137a0c2028abf2d2
emanuelefavero <emafaux89@gmail.com>
2022-10-27 09:58:29 +0200

    add validate and sanitize data before registering

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
+const { body, validationResult } = require('express-validator')
 
 const User = require('../models/user')
 
@@ -16,27 +17,73 @@ exports.login = (req, res, next) => {
     }
   })(req, res, next)
 }
-exports.register = (req, res) => {
-  User.findOne({ username: req.body.username }, async (err, doc) => {
-    if (err) throw err
-    if (doc) res.send('User Already Exists')
-    if (!doc) {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10)
-      const newUser = new User({
-        username: req.body.username,
-        password: hashedPassword,
-        email: req.body.email,
-        fullName: req.body.fullName,
-        role: req.body.role,
-        membership: req.body.membership,
-        createdAt: req.body.registeredSince,
+
+// Validate the user's input and register a new user
+exports.register = [
+  // Validate and sanitize fields.
+  body('username', 'Username must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('password', 'Password must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('email', 'Email must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('fullName', 'Full name must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .optional({ checkFalsy: true }),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.send(errors.array())
+      return
+    } else {
+      // Data from form is valid.
+      // Check if User with same username already exists.
+      User.findOne({ username: req.body.username }).exec((err, foundUser) => {
+        if (err) {
+          return next(err)
+        }
+
+        if (foundUser) {
+          // User exists, redirect to its detail page.
+          res.send('User already exists')
+        } else {
+          bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+            if (err) {
+              return next(err)
+            }
+            const user = new User({
+              username: req.body.username,
+              password: hashedPassword,
+              email: req.body.email,
+              fullName: req.body.fullName,
+              // role: req.body.role,
+              // membership: req.body.membership,
+              // createdAt: req.body.registeredSince,
+            }).save((err) => {
+              if (err) {
+                return next(err)
+              }
+              res.send('User created successfully')
+            })
+          })
+        }
       })
-      await newUser.save()
-      res.send('User Created')
     }
-  })
-  // console.log(req.body)
-}
+  },
+]
 
 // Logout - destroys the session
 exports.logout = (req, res) => {
